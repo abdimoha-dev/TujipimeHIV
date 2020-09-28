5678056a0ffc174eaae080d9013b698e0d77767f
abdy254 <abdymohammed@gmail.com>
2020-09-28 03:03:32 +0300

    Update users management

--- hivtests/urls.py
+++ hivtests/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('add', views.add_test, name="add"),
-    path('results', views.get_daily_results, name='results')
+    path('results', views.get_daily_results, name='results'),
+    path('accounts/', include("django.contrib.auth.urls"), name= "accounts"),
+    path('login', views.login, name = 'login'),
+    path('logout', views.logout, name ='logout')
 ]
--- hivtests/views.py
+++ hivtests/views.py
@@ -1,17 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tests
 from .forms import NameForm
 from datetime import datetime
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout as django_logout
 
 
+def login(request):
+    
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user =authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('add')
+        
+    else:
+        return HttpResponse('/you are not logged in/')
+            
+
+
+
+def logout(request):
+    django_logout(request)
+    return HttpResponse('/you have logged out')
 def index(request):
-    all_tests = Tests.objects.order_by('date_tested')[:5]
-    context = {'all_tests': all_tests}
-    return render(request, "tests.html", context)
-# @login_required
+    # all_tests = Tests.objects.order_by('date_tested')[:5]
+    # context = {'all_tests': all_tests}
+    return render(request, "base.html")
 
-
+@login_required
 def add_test(request):
     if request.method == 'POST':
         form = NameForm(request.POST)
